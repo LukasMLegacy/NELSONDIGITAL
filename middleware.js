14d8f6b1f14a777ad5b1f82579ceda6bc7dd3c2a
LukasMLegacy <148592758+LukasMLegacy@users.noreply.github.com>
2026-05-11 16:04:42 +0200

    f

---
+++ middleware.js
@@ -0,0 +1,28 @@
+import { next, rewrite } from '@vercel/functions';
+
+const NELSON_HOSTS = new Set(['nelsondigital.shop', 'www.nelsondigital.shop']);
+
+export default function middleware(request) {
+  const url = new URL(request.url);
+  if (!NELSON_HOSTS.has(url.hostname.toLowerCase())) {
+    return next();
+  }
+
+  const base = '/sites/nelsondigital';
+  let pathname = url.pathname;
+  if (pathname.startsWith(`${base}/`) || pathname === base) {
+    return next();
+  }
+  if (pathname === '/' || pathname === '') {
+    pathname = '/index.html';
+  }
+
+  const dest = new URL(`${base}${pathname}`, url.origin);
+  dest.search = url.search;
+  dest.hash = url.hash;
+  return rewrite(dest);
+}
+
+export const config = {
+  matcher: ['/', '/((?!_next).*)'],
+};
